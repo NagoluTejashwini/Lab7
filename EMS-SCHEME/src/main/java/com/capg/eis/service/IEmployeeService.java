bc0050a38517d74b489656d6934804a90c75aa73
Nagolu Tejashwini <52523493+NagoluTejashwini@users.noreply.github.com>
2021-04-21 17:57:12 +0530

    Add files via upload

---
+++ EMS-SCHEME/src/main/java/com/capg/eis/service/IEmployeeService.java
@@ -0,0 +1,12 @@
+package com.capg.eis.service;
+
+import java.util.HashMap;
+import java.util.List;
+
+import com.capg.eis.bean.Employee;
+
+public interface IEmployeeService {
+	public boolean insertEmployee(Employee e, String scheme);
+	public HashMap<Employee, String> getAllEmployees();
+	public List<Employee> getAllEmployeesByScheme(String Scheme);
+}
